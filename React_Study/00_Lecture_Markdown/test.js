0de7e72f6ecc2e129d98f7259e8819a0fcbfb748
siwon-park <zow777@naver.com>
2022-06-30 19:05:37 +0900

    React_Study upload

---
+++ React_Study/00_Lecture_Markdown/test.js
@@ -0,0 +1,6 @@
+// const {name} = {name:'siwon', age: 30}
+// console.log(name)
+
+const { name, age } = { name: 'siwon', age: 30 }
+console.log(name, age)
+
---
+++ React_Study/01_Practice/01_my-react-app/src/App.js
@@ -0,0 +1,13 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  // 선언형 방식의 코딩
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
