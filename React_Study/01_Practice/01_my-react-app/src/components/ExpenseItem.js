ee0106894c377585742f541c27a20a244d9c0b40
siwon-park <zow777@naver.com>
2022-07-02 01:21:54 +0900

    220701 SW: React Study upload

--- React_Study/01_Practice/01_my-react-app/src/components/ExpenseItem.js
+++ React_Study/01_Practice/01_my-react-app/src/components/ExpenseItem.js
@@ -1,6 +1,18 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
+  const expenseDate = new Date()
+  const expenseTitle = 'Car Insurance'
+  const expenseAmount = 294.67
+
   return (
-    <h2>ExpenseItem!</h2>
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>${expenseAmount}</div>
+      </div>
+    </div>
   )
 }
 
